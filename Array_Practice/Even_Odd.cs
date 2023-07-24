8696b676404498e98eab82dafb7deb4f9bf91105
vidyashreehipparagi <hipparagividyashree14@gmail.com>
2023-07-24 13:51:00 +0530

    Add project files.

---
+++ Array_Practice/Even_Odd.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Practice
+{
+    internal class Even_Odd
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            int j[5], k = 0;
+            Console.WriteLine("Enter elemts of an array");
+            for(int i=0;i<arr.Length-1;i++) {
+                arr[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            
+            //Console.WriteLine("Even ");
+            for (int i=0;i<arr.Length;i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    arr[j++] = arr[i];
+                   
+                }
+                else
+                {
+                    k++;
+                }
+            }
+          
+                Console.WriteLine("Even"+arr[j]);
+           //
+                //Console.WriteLine("odd" + arr[k]);
+
+            
+
+          
+        }
+    }
+}
---
+++ Array_Practice/Max_MIn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Practice
+{
+    internal class Max_MIn
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            Console.WriteLine("Enter number");
+           // int n=Convert.ToInt32(Console.ReadLine());
+            for(int i=0; i<arr.Length; i++)
+            {
+                arr[i]= Convert.ToInt32(Console.ReadLine());
+            }
+            int max = arr[0];
+            int min= arr[0];
+            for(int i=0;i<arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                else if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine($"Maximum Value is {max}");
+            Console.WriteLine($"Minimum value is {min}");
+        }
+    }
+}
